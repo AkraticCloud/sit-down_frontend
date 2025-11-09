f0a971ccb6b005857d0907b4c8a45e5464b6f1d3
Adonay Lisanework <adonaylisanework@gmail.com>
2025-11-08 20:35:19 -0500

    implemented app home page with animations

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,10 @@ import '@material/web/textfield/outlined-text-field.js';
 import '@material/web/checkbox/checkbox.js';
 import { styles as typescaleStyles } from '@material/web/typography/md-typescale-styles.js';
 import './App.css';
+import AppHomePage from '/src/components/AppHomePage.jsx';
 
 function App() {
-  useEffect(() => {
-    // Apply Material Design typography styles
-    document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
-  }, []);
-
-  return (
-    <div className="app">
-      <h1 className="md-typescale-display-medium">Hello Material!</h1>
-      
-      <form className="form">
-        <p className="md-typescale-body-medium">Check out these controls in a form!</p>
-        
-        <md-checkbox></md-checkbox>
-        
-        <md-outlined-text-field 
-          label="Favorite color" 
-          value="Purple"
-        ></md-outlined-text-field>
-        
-        <div className="button-group">
-          <md-filled-button type="button">Submit</md-filled-button>
-          <md-outlined-button type="reset">Reset</md-outlined-button>
-        </div>
-      </form>
-    </div>
-  );
+  return <AppHomePage />;
 }
 
 export default App;
