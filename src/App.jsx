3a6de91208aefa48186b731a86f50aef9854f5c5
Gray <mitchellbadri@gmail.com>
2025-11-09 23:52:21 -0500

    Added basic routing. Did some minor styling adjustments to improve site appearance. Fixed a minor issue with the try/catch on line 61 in the home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/textfield/outlined-text-field.js';
 import '@material/web/checkbox/checkbox.js';
 import './App.css';
 import GetStartedPage from './components/GetStartedPage';
+import RegistrationPage from './components/RegistrationPage'
+import HomePage from './components/AppHomePage'
+import SettingsPage from './components/SettingsPage'
 
 function App() {
-  return <GetStartedPage />;
+  // return <GetStartedPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<GetStartedPage />}/>
+        <Route path='/registration' element={<RegistrationPage />}/>
+        <Route path='/home' element={<HomePage />}/>
+        <Route path='/settings' element={<SettingsPage />}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
