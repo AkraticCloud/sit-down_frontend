da2489aed3f63b40ec8fc614ebfe60f773f0e418
Gray <mitchellbadri@gmail.com>
2025-11-09 23:23:18 -0500

    Set the GetStartedPage as the landing page for the app.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import '@material/web/button/outlined-button.js';
 import '@material/web/textfield/outlined-text-field.js';
 import '@material/web/checkbox/checkbox.js';
 import './App.css';
-import AppHomePage from '/src/components/AppHomePage.jsx';
+import GetStartedPage from './components/GetStartedPage';
 
 function App() {
-  return <AppHomePage />;
+  return <GetStartedPage />;
 }
 
 export default App;
