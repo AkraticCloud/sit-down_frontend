2e2ee69fe479b855927c89489e8d4dcd08257b3a
simulati0n <tomthen4@gmail.com>
2025-12-07 16:43:06 -0500

    Extracted svg logo and improved CSS

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,7 @@
 
 function Logo(){
     return (
-        <div className="logo-container">
+        <div className={`logo-container ${className}`}>
             <div className="logo-circle">
                 <svg className="logo-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path
