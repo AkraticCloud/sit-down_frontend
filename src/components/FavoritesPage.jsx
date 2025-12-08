8f0a01a20d63c813cbfd95fd09435d88cca18b96
Gray <mitchellbadri@gmail.com>
2025-12-07 22:41:20 -0500

    Filled favorites page with toy data pending debug of backend connection.

--- src/components/FavoritesPage.jsx
+++ src/components/FavoritesPage.jsx
@@ -5,89 +5,128 @@ import '@material/web/divider/divider.js';
 
 function FavoritesPage(){
     const [currentList, setCurrentList] = useState(0)
-    const [currentListData, setCurrentListData] = useState(null)
-    const [foodListData, setFoodListData] = useState(null)
-    const tempUname = "test101x9"
+    //TODO - Implement these to pull data from the backend. 
+    // const [currentListData, setCurrentListData] = useState(null)
+    // const [foodListData, setFoodListData] = useState(null)
+    // const tempUname = "test101x9"
     const toyListArr = useMemo(() => {
-        return([{"foodlist-name": "1"}, {"foodlist-name": "2"}, {"foodlist-name": "3"}])
+        return([
+            {
+                foodlistname: "Favorites",
+                restaurants: ["Daily Sushi", "Tikka Shack", "Hip Hop Fish & Chicken"]
+            }, 
+            {
+                foodlistname: "Pizza",
+                restaurants: ["Cheesy Pizza", "Dominos", "Pizza Hut"]
+            }, 
+            {
+                foodlistname: "Sushi",
+                restaurants: ["Daily Sushi", "Charm City Buffet & Grill", "Sushi Hana"]
+            },
+            {
+                foodlistname: "Test1",
+                restaurants: ["Test1.1", "Test1.2", "Test1.3", "Test1.4", "Test1.5", "Test1.6", "Test1.7", "Test1.8", "Test1.9", "Test1.10", "Test1.11", "Test1.12", "Test1.13", "Test1.14"]
+            },
+            {
+                foodlistname: "Test2",
+                restaurants: []
+            },
+            {
+                foodlistname: "Toy List",
+                restaurants: ["Daily Sushi", "Charm City Buffet & Grill", "Sushi Hana"]
+            },
+            {
+                foodlistname: "Toy List",
+                restaurants: ["Daily Sushi", "Charm City Buffet & Grill", "Sushi Hana"]
+            },
+            {
+                foodlistname: "Toy List",
+                restaurants: ["Daily Sushi", "Charm City Buffet & Grill", "Sushi Hana"]
+            },
+            {
+                foodlistname: "Toy List",
+                restaurants: ["Daily Sushi", "Charm City Buffet & Grill", "Sushi Hana"]
+            },  
+        ])
     }, [])
 
     useEffect(() => {
         document.adoptedStyleSheets.push(typescaleStyles.styleSheet);
     }, []);
 
-    useEffect(() =>{
-        const createList = async (foodlist_name, restaurant_id, username) => {
-            try{
-                const response = await fetch(`https://sit-down-backend.vercel.app/action/createlist`, {
-                    method: "POST",
-                    body: JSON.stringify({
-                        foodlist_name: foodlist_name,
-                        restaurant_id: restaurant_id,
-                        username: username
-                    })
-                })
+    //TODO - Implement these effects to load the user's lists from the backend and load the current lists data. 
+    // useEffect(() =>{
+    //     const createList = async (foodlist_name, restaurant_id, username) => {
+    //         try{
+    //             const response = await fetch(`https://sit-down-backend.vercel.app/action/createlist`, {
+    //                 method: "POST",
+    //                 body: JSON.stringify({
+    //                     foodlist_name: foodlist_name,
+    //                     restaurant_id: restaurant_id,
+    //                     username: username
+    //                 })
+    //             })
 
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
+    //             if (!response.ok) {
+    //                 throw new Error(`HTTP error! status: ${response.status}`);
+    //             }
 
-                const data = await response.json()
-                console.log("Data returned:" + data)
-            }catch (e){
-                console.log(e)
-            }
-        }
+    //             const data = await response.json()
+    //             console.log("Data returned:" + data)
+    //         }catch (e){
+    //             console.log(e)
+    //         }
+    //     }
 
-        createList(toyListArr[0]["foodlist-name"], "test", tempUname)
-        createList(toyListArr[1]["foodlist-name"], "test", tempUname)
-        createList(toyListArr[2]["foodlist-name"], "test", tempUname)
-    }, [toyListArr])
+    //     createList(toyListArr[0]["foodlist-name"], "test", tempUname)
+    //     createList(toyListArr[1]["foodlist-name"], "test", tempUname)
+    //     createList(toyListArr[2]["foodlist-name"], "test", tempUname)
+    // }, [toyListArr])
 
-    //Potentially refactor to remove the function assignments, just execute the code in the useEffect
-    useEffect(() => { //Grabs the foodlists the user has made. 
-        const fetchLists = async () => {
-            try{
-                const response = await fetch(`https://sit-down-backend.vercel.app/action/foodlists/:${tempUname}`)
+    // //Potentially refactor to remove the function assignments, just execute the code in the useEffect
+    // useEffect(() => { //Grabs the foodlists the user has made. 
+    //     const fetchLists = async () => {
+    //         try{
+    //             const response = await fetch(`https://sit-down-backend.vercel.app/action/foodlists/:${tempUname}`)
 
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
-                }
+    //             if (!response.ok) {
+    //                 throw new Error(`HTTP error! status: ${response.status}`);
+    //             }
 
-                const data = await response.json()
+    //             const data = await response.json()
 
-                setFoodListData(data)
-                console.log("Data returned:" + data)
-            }catch (e){
-                console.log(e)
-            }
-        }
+    //             setFoodListData(data)
+    //             console.log("Data returned:" + data)
+    //         }catch (e){
+    //             console.log(e)
+    //         }
+    //     }
 
-        fetchLists()
-    }, []);
+    //     fetchLists()
+    // }, []);
 
-    useEffect(() =>{ //Grabs the contents of the current foodlist. 
-        const fetchRestaurants = async () => {
-            if(currentList != null){
-                try{
-                    const response = await fetch(`https://sit-down-backend.vercel.app/action/foodlists/:${tempUname}/:${currentList}`)
+    // useEffect(() =>{ //Grabs the contents of the current foodlist. 
+    //     const fetchRestaurants = async () => {
+    //         if(currentList != null){
+    //             try{
+    //                 const response = await fetch(`https://sit-down-backend.vercel.app/action/foodlists/:${tempUname}/:${currentList}`)
 
-                    if (!response.ok) {
-                        throw new Error(`HTTP error! status: ${response.status}`);
-                    }
+    //                 if (!response.ok) {
+    //                     throw new Error(`HTTP error! status: ${response.status}`);
+    //                 }
 
-                    const data = await response.json()
+    //                 const data = await response.json()
 
-                    setCurrentListData(data)
-                    console.log("Data returned:" + data)
-                }catch (e){
-                    console.log(e)
-                }
-            }
-        }
+    //                 setCurrentListData(data)
+    //                 console.log("Data returned:" + data)
+    //             }catch (e){
+    //                 console.log(e)
+    //             }
+    //         }
+    //     }
 
-        fetchRestaurants()
-    }, [currentList]); //Retriggers this effect when the current list changes. 
+    //     fetchRestaurants()
+    // }, [currentList]); //Retriggers this effect when the current list changes. 
 
     return(
         <div className="favorites-page">
@@ -102,14 +141,21 @@ function FavoritesPage(){
                         }
                     >
                         <section className="foodlist-row-content">
-                            <h3>{list["foodlist-name"]}</h3>
+                            <h3>{list.foodlistname}</h3>
                         </section>
                     </section>
                 ))}
             </section>
             <section className="current-foodlist-container">
-                <h1>{currentList}</h1>
+                <h1>{toyListArr[currentList].foodlistname}</h1>
                 <md-divider></md-divider>
+                {toyListArr[currentList].restaurants.map((list) => (
+                    <section className="foodlist-row">
+                        <section className="foodlist-row-content">
+                            <h3>{list}</h3>
+                        </section>
+                    </section>
+                ))}
             </section>
         </div>
     )
